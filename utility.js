8d07fe424f049b805b931e5eedd5d1b9d58d1344
nitis <nitish8152@gmail.com>
2022-12-10 07:49:41 +0530

    Added:-Employee-Payroll-Home-Page

---
+++ utility.js
@@ -0,0 +1,6 @@
+onst stringifyDate = (date) => {
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    const newDate = !date ? "undefined" :
+        new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
+    return newDate;
+}
